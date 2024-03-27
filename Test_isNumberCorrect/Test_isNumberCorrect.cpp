2b37b0ffac1f45e9635a06345e54d7749cfead2f
Burkin Nikita <nik.burk.0403@gmail.com>
2024-03-27 19:18:27 +0300

    Создал тесты для функции isNumberCorrect

--- Test_isNumberCorrect/Test_isNumberCorrect.cpp
+++ Test_isNumberCorrect/Test_isNumberCorrect.cpp
@@ -9,9 +9,41 @@ namespace TestisNumberCorrect
 	TEST_CLASS(TestisNumberCorrect)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(SampleTest)
 		{
+			ErrorCode code = isNumberCorrect(20);
+			Assert::AreEqual(static_cast<int>(code), static_cast<int>(ErrorCode::NoError));
+		}
+
+		TEST_METHOD(NumberInRangeOfAcceptableValues)
+		{
+			ErrorCode code = isNumberCorrect(6);
+			Assert::AreEqual(static_cast<int>(code), static_cast<int>(ErrorCode::NoError));
+		}
+
+		TEST_METHOD(NumberOutsideMaximumLimit)
+		{
+			ErrorCode code = isNumberCorrect(101);
+			Assert::AreEqual(static_cast<int>(code), static_cast<int>(ErrorCode::IncorrectNumber));
+		}
+
+		TEST_METHOD(NumberIsZero)
+		{
+			ErrorCode code = isNumberCorrect(0);
+			Assert::AreEqual(static_cast<int>(code), static_cast<int>(ErrorCode::IncorrectNumber));
+		}
+
+		TEST_METHOD(CouldNotFoundOpenInputFile)
+		{
+			ErrorCode code = isNumberCorrect(-2);
+			Assert::AreEqual(static_cast<int>(code), static_cast<int>(ErrorCode::IncorrectInputFile));
+		}
+
+		TEST_METHOD(DataIsNotNaturalNumber)
+		{
+			ErrorCode code = isNumberCorrect(-1);
+			Assert::AreEqual(static_cast<int>(code), static_cast<int>(ErrorCode::IncorrectInputData));
 		}
 	};
 }
