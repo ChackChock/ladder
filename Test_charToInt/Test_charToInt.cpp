e3d6a4da6128fa02f0b9f930eeefd02380827668
Burkin Nikita <nik.burk.0403@gmail.com>
2024-03-27 18:50:42 +0300

    Создал тесты для функции charToInt

--- Test_charToInt/Test_charToInt.cpp
+++ Test_charToInt/Test_charToInt.cpp
@@ -9,9 +9,34 @@ namespace TestcharToInt
 	TEST_CLASS(TestcharToInt)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(SampleTest)
 		{
+			int result = charToInt("5-3");
+			Assert::AreEqual(result, -1);
+		}
+
+		TEST_METHOD(ArrayStartsWithMinus)
+		{
+			int result = charToInt("-53");
+			Assert::AreEqual(result, -1);
+		}
+
+		TEST_METHOD(MinusInMiddleOfArray)
+		{
+			int result = charToInt("55-31");
+			Assert::AreEqual(result, -1);
+		}
+
+		TEST_METHOD(MinusAtEndOfArray)
+		{
+			int result = charToInt("53-");
+			Assert::AreEqual(result, -1);
+		}
+		TEST_METHOD(ArrayConsistsOfDigits)
+		{
+			int result = charToInt("53");
+			Assert::AreEqual(result, 53);
 		}
 	};
 }
