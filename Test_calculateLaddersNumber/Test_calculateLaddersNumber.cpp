e065761b02040ade98feccbb8e1ead5015daebfd
Burkin Nikita <nik.burk.0403@gmail.com>
2024-03-27 17:35:09 +0300

    Создал файл заголовка и 3 проекта для модульных тестов

---
+++ Test_calculateLaddersNumber/Test_calculateLaddersNumber.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestcalculateLaddersNumber
+{
+	TEST_CLASS(TestcalculateLaddersNumber)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ Test_charToInt/Test_charToInt.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestcharToInt
+{
+	TEST_CLASS(TestcharToInt)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
