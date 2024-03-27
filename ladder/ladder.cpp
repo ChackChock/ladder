c6f3984fd54b323be482b0ab83be62748c7d3f8d
Burkin Nikita <nik.burk.0403@gmail.com>
2024-03-27 20:18:55 +0300

    Реализовал функцию main

--- ladder/ladder.cpp
+++ ladder/ladder.cpp
@@ -7,7 +7,38 @@ using namespace std;
 
 int main(const int argc, char** argv)
 {
+	setlocale(LC_ALL, "Russian");
+	if (argc != 3) {
+		cout << "Не указаны файл(-ы) с входными и(или) выходными данными." << endl;
+		return 1;
+	}
 
+	int number = readNumberFromFile(argv[1]);
+	ErrorCode code = isNumberCorrect(number);
+	int error_number = static_cast<int>(code);
+
+	if (error_number == 1) {
+		cout << "Неверно указан файл с входными данными. Возможно, файл не существует." << endl;
+		return 1;
+	}
+	if (error_number == 2) {
+		cout << "Неверно записаны входные данные. В файле не должны быть не цифровые значения." << endl;
+		return 1;
+	}
+	if (error_number == 3) {
+		cout << "Неверно записаны входные данные. Файл должен содержать натуральное число от 1 до 100 включительно." << endl;
+		return 1;
+	}
+
+	int ladders_number = calculateLaddersNumber(number + 1, number);
+	code = writeResultToFile(ladders_number, argv[2]);
+	error_number = static_cast<int>(code);
+
+	if (error_number == 4) {
+		cout << "Неверно указан файл для выходных данных. Возможно, указанного расположения не существует или нет прав на запись." << endl;
+	}
+
+	return 0;
 }
 
 
