c235da55786275fd1a8327225f19f7f5b69b9fa1
Burkin Nikita <nik.burk.0403@gmail.com>
2024-03-27 18:08:50 +0300

    Реализовал функцию calculateLaddersNumber.

--- ladder/ladder.cpp
+++ ladder/ladder.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#include "Header.h"
+#include <iostream>
 #include <fstream>
 
 using namespace std;
@@ -7,4 +8,17 @@ using namespace std;
 int main(const int argc, char** argv)
 {
 
+}
+
+
+int calculateLaddersNumber(int prev_level, int number) {
+	if (number == 0) return 1;
+
+	int count = 0;
+	for (int level = 1; level < prev_level; ++level) {
+		if (level > number) break;
+		count += calculateLaddersNumber(level, number - level);
+	}
+
+	return count;
 }
